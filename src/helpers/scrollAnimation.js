6eedc1b76b3417e9e5fee3a24e9d4dc3a2b806d4
Aslanov Rustam <aslanovrg@gmail.com>
2023-07-20 17:39:34 +0300

    fix scroll anim issue

--- src/helpers/scrollAnimation.js
+++ src/helpers/scrollAnimation.js
@@ -1,32 +1,76 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import s from "./scrollAnimation.module.css";
 
 const ScrollAnimation = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false);
-  const ref = useRef(null);
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (ref.current) {
-        const top = ref.current.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-        setIsVisible(top < windowHeight);
+    const handleIntersection = (entries) => {
+      if (entries[0].isIntersecting) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    handleScroll(); // Проверяем видимость блока при первой загрузке
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.1,
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
     };
   }, []);
 
   return (
-    <div ref={ref} className={`${s.animation} ${isVisible ? s.visible : ""}`}>
-      {children}
+    <div ref={containerRef}>
+      {React.Children.map(children, (child, index) => (
+        <div
+          key={index}
+          className={`${s.animation} ${isVisible ? s.visible : ""}`}
+        >
+          {child}
+        </div>
+      ))}
     </div>
   );
 };
 
 export default ScrollAnimation;
+
+// const ScrollAnimation = ({ children }) => {
+//   const [isVisible, setIsVisible] = useState(false);
+//   const ref = useRef(null);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (ref.current) {
+//         const top = ref.current.getBoundingClientRect().top;
+//         const windowHeight = window.innerHeight;
+//         setIsVisible(top < windowHeight);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+//     handleScroll(); // Проверяем видимость блока при первой загрузке
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div ref={ref} className={`${s.animation} ${isVisible ? s.visible : ""}`}>
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default ScrollAnimation;
